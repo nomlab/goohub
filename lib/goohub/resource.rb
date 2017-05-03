5442d282681b185cca8a5c290b71f09e4974614d
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2017-05-03 17:00:15 +0900

    Implement EventCollection for Events

--- lib/goohub/resource.rb
+++ lib/goohub/resource.rb
@@ -10,10 +10,24 @@ module Goohub
       end
     end # class Base
 
+    class Collection
+      extend Forwardable
+      extend Enumerable
+
+      def initialize(raw_resources)
+        @raw_resources = raw_resources
+      end
+
+      # required to implement in subclass for enumerable methods
+      def each
+        raise 'Not implemented'
+      end
+    end # class Collection
+
     dir = File.dirname(__FILE__) + "/resource"
 
     autoload :Event,             "#{dir}/event.rb"
     autoload :Calendar,          "#{dir}/calendar.rb"
-    autoload :Events,            "#{dir}/events.rb"
+    autoload :EventCollection,   "#{dir}/event_collection.rb"
   end # module Resource
 end # module Goohub
