c64135061f3369d3a528dad5b0da1da211b04ca3
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2017-10-27 16:13:16 +0900

    Design share command in detail
    Add 4 function in share command.
    Make default sentense itemse, Apply filter, Make sentense, and Apply action.

--- lib/goohub/command/share.rb
+++ lib/goohub/command/share.rb
@@ -1,15 +1,51 @@
 class GoohubCLI < Clian::Cli
   desc "share CALENDAR_ID EVENT_ID", "Share event by EVENT_ID"
 
-  def share(calendar_id, event_id)
+  def share(calendar_id, event_id, filter = "location_delete", action = "stdout")
     event = client.get_event(calendar_id, event_id)
-    puts "summary:    #{event.summary}\n"
-    puts "id:         #{event.id}\n"
-    puts "created:    #{event.created}\n"
-    puts "kind:       #{event.kind}\n"
-    puts "organized:  #{event.organizer.display_name}\n"
-    puts "start_time: #{event.start.date_time}\n"
-    puts "end_time:   #{event.end.date_time}\n"
-    puts "location:   #{event.location}\n"
+    sentence_items = make_default_sentence_items(event)
+    sentence_items = apply_filter(sentence_items, filter)
+    sentence = make_sentence(sentence_items)
+    apply_action(sentence, action)
   end
-end
+
+  private
+
+  def make_default_sentence_items(event)
+    sentence_items = {}
+    sentence_items["summary"] =    event.summary
+    sentence_items["id"] =         event.id
+    sentence_items["created"] =    event.created
+    sentence_items["kind"] =       event.kind
+    sentence_items["organized"] =  event.organizer.display_name
+    sentence_items["start_time"] = event.start.date_time
+    sentence_items["end_time"] =   event.end.date_time
+    sentence_items["location"] =   event.location
+    sentence_items
+  end
+
+  def apply_filter(sentence_items, filter)
+    sentence_items["location"] = nil if filter == "location_delete"
+    sentence_items["created"] = nil if filter == "created_delete"
+    sentence_items["summary"] = nil if filter == "summary_delete"
+    sentence_items
+  end
+
+  def make_sentence(sentence_items)
+    sentence = ""
+    sentence_items.each{ |key, value|
+      item_name = "#{key}:"
+      # This while is need to align indent
+      while item_name.length < 17
+        item_name << " "
+      end
+
+      sentence <<  item_name + "#{value}" + "\n"
+    }
+    sentence
+  end
+
+  def apply_action(sentence, action)
+    puts sentence if action == "stdout"
+  end
+end# class GoohubCLI
