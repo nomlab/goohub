39dbdd3f620dbd4410cedceef0f468eacf0a771e
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2017-12-12 21:03:02 +0900

    Adjust code to easier read

--- lib/goohub/command/share.rb
+++ lib/goohub/command/share.rb
@@ -14,34 +14,24 @@ If you use `slack` in ACTION, you need get incoming-webhook url and set it in se
 LONGDESC
 
   def share(calendar_id, event_id, filter = "no_filter", action = "stdout")
-    @incoming_webhook = ""
-    @sentence_items = {}
-    @sentence = ""
-    @event = client.get_event(calendar_id, event_id)
-    set_incoming_webhook_url
-    make_default_sentence_items
+    event = client.get_event(calendar_id, event_id)
+    parse_event(event)
     apply_filter(filter)
-    make_sentence
     apply_action(action)
   end
 
   private
 
-  def set_incoming_webhook_url
-    settings_file_path = "settings.yml"
-    config = YAML.load_file(settings_file_path) if File.exist?(settings_file_path)
-    @incoming_webhook = ENV['INCOMING_WEBHOOK_URL'] || config["incoming_webhook_url"]
-  end
-
-  def make_default_sentence_items
-    @sentence_items["summary"] =    @event.summary
-    @sentence_items["id"] =         @event.id
-    @sentence_items["created"] =    @event.created
-    @sentence_items["kind"] =       @event.kind
-    @sentence_items["organized"] =  @event.organizer.display_name
-    @sentence_items["start_time"] = @event.start.date_time
-    @sentence_items["end_time"] =   @event.end.date_time
-    @sentence_items["location"] =   @event.location
+  def parse_event(event)
+    @sentence_items = {}
+    @sentence_items["summary"] =    event.summary
+    @sentence_items["id"] =         event.id
+    @sentence_items["created"] =    event.created
+    @sentence_items["kind"] =       event.kind
+    @sentence_items["organized"] =  event.organizer.display_name
+    @sentence_items["start_time"] = event.start.date_time
+    @sentence_items["end_time"] =   event.end.date_time
+    @sentence_items["location"] =   event.location
   end
 
   def apply_filter(filter)
@@ -51,26 +41,24 @@ LONGDESC
     @sentence_items["summary"] = nil if filter == "summary_delete"
   end
 
-  def make_sentence
-    @sentence_items.each{ |key, value|
-      item_name = "#{key}:"
-      # This while is need to align indent
-      while item_name.length < 17
-        item_name << " "
-      end
-      @sentence <<  item_name + "#{value}" + "\n"
-    }
-  end
-
   def apply_action(action)
-    puts @sentence if action == "stdout"
-    post_slack(@sentence) if action == "slack"
+    puts make_sentence if action == "stdout"
+    post_slack if action == "slack"
     post_calendar(action.partition(":")[2]) if action.include?("calendar")
   end
 
-  def post_slack(string, options = {})
-    payload = options.merge({text: string})
-    uri = URI.parse(@incoming_webhook)
+  def make_sentence
+    sentence = ""
+    @sentence_items.each{ |key, value|
+      sentence <<  "#{key}: #{value}\n"
+    }
+    sentence
+  end
+
+  def post_slack(options = {})
+    sentence = make_sentence
+    payload = options.merge({text: sentence})
+    uri = URI.parse(set_incoming_webhook_url)
     res = nil
     json = payload.to_json
     request = "payload=" + json
@@ -80,8 +68,14 @@ LONGDESC
       res = http.post(uri.request_uri, request)
     end
     puts "Slack post is end, post content is under"
-    puts  @sentence
+    puts  sentence
     return res
+  end
+
+  def set_incoming_webhook_url
+    settings_file_path = "settings.yml"
+    config = YAML.load_file(settings_file_path) if File.exist?(settings_file_path)
+    incoming_webhook = ENV['INCOMING_WEBHOOK_URL'] || config["incoming_webhook_url"]
   end
 
   def post_calendar(calendar_id)
