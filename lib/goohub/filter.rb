55311c52e74e55253917dc2b4141c4d4cfd71fab
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-02-08 12:46:44 +0900

    Divided process of Filter class into condition part and editing part

--- lib/goohub/filter.rb
+++ lib/goohub/filter.rb
@@ -1,16 +1,46 @@
 module Goohub
   class Filter
-    def initialize(name, sentence_items)
-      @name = name
+    def initialize(filter_id, sentence_items)
+      @filter_id = filter_id
       @sentence_items = sentence_items
+      filter = Struct.new("Filter", :condition, :modifier)
+      @summary_delete = filter.new("true","replace:summary:nil")
+      @created_delete = filter.new("true","replace:created:nil")
+      @location_delete = filter.new("true","replace:location:nil")
     end
 
     def apply
-      return if @name == "no_filter"
-      @sentence_items["location"] = nil if @name== "location_delete"
-      @sentence_items["created"] = nil if @name == "created_delete"
-      @sentence_items["summary"] = nil if @name == "summary_delete"
+      return if @filter_id == "no_filter"
+      if confirm_condition == true
+        modify_event
+      end
       @sentence_items
+    end
+
+    private
+
+    #####################################################
+    ### codition_methods
+    #####################################################
+    def confirm_condition
+      eval(eval("@#{@filter_id}.condition"))
+    end
+
+    #####################################################
+    ### modify_methods
+    #####################################################
+    def modify_event
+      method = eval("@#{@filter_id}.modifier.split(':')[0]")
+      field = eval("@#{@filter_id}.modifier.split(':')[1]")
+      value = eval("@#{@filter_id}.modifier.split(':')[2]")
+      eval("#{method}(field,#{value})")
+    end
+
+    #####################################################
+    ### templete_methods
+    #####################################################
+    def replace(field, str)
+      @sentence_items["#{field}"] = str
     end
   end# class Filter
 end# module Goohub
