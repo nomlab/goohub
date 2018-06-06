200089b877e6ce85ffead5f15f4ba4b0f365352d
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-06-07 00:51:31 +0900

    Mix condition part and editing part into expand_query in Filter class

--- lib/goohub/filter.rb
+++ lib/goohub/filter.rb
@@ -1,39 +1,30 @@
+# coding: utf-8
 module Goohub
   class Filter
     def initialize(filter_id, sentence_items)
       @filter_id = filter_id
       @sentence_items = sentence_items
-      filter = Struct.new("Filter", :condition, :modifier)
-      @summary_delete = filter.new("true","replace:summary:nil")
-      @created_delete = filter.new("true","replace:created:nil")
-      @location_delete = filter.new("true","replace:location:nil")
+      filter = Struct.new("FilterID", :condition, :modifier)
+      @summary_delete  = filter.new("match:summary:.","replace:summary:")
+      @created_delete  = filter.new("match:summary:.","replace:created:")
+      @location_delete = filter.new("match:summary:.","replace:location:")
     end
 
     def apply
       return if @filter_id == "no_filter"
-      if confirm_condition == true
-        modify_event
+      if expand_query("condition") != nil && expand_query("condition") != false
+        expand_query("modifier")
       end
       @sentence_items
     end
 
     private
 
-    #####################################################
-    ### codition_methods
-    #####################################################
-    def confirm_condition
-      eval(eval("@#{@filter_id}.condition"))
-    end
-
-    #####################################################
-    ### modify_methods
-    #####################################################
-    def modify_event
-      method = eval("@#{@filter_id}.modifier.split(':')[0]")
-      field = eval("@#{@filter_id}.modifier.split(':')[1]")
-      value = eval("@#{@filter_id}.modifier.split(':')[2]")
-      eval("#{method}(field,#{value})")
+    def expand_query(type)
+      method = eval("@#{@filter_id}.#{type}.split(':')[0]")
+      field = eval("@#{@filter_id}.#{type}.split(':')[1]")
+      value = eval("@#{@filter_id}.#{type}.split(':')[2]")
+      eval("#{method}(field,value)")
     end
 
     #####################################################
@@ -41,6 +32,14 @@ module Goohub
     #####################################################
     def replace(field, str)
       @sentence_items["#{field}"] = str
+    end
+
+    def match(field, pattern)
+      @sentence_items["#{field}"].match(pattern)
+    end
+
+    def include?(field, str)
+      @sentence_items["#{field}"].include?(str)
     end
   end# class Filter
 end# module Goohub
