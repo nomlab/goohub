55cdf073abb45a674e0a360bb5b899fc1c76ba4d
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-06-11 19:47:47 +0900

    Buit in DataStore class to Filter class
    DataStoreクラスのredisを利用し，フィルタを保存できるようにした．
    また，フィルタ適用時には，利用するフィルタを読みだして使うようにした．
    redisはデフォルトのポートを前提としている．
    TODO: filter_idの検討
    システムが扱うidと，ユーザが扱うidを分けるか，また，同じ名前はどうするか，など
    TODO: dbのフィルタに対する操作の実装(list, edit, create, delete)

--- lib/goohub/filter.rb
+++ lib/goohub/filter.rb
@@ -4,10 +4,8 @@ module Goohub
     def initialize(filter_id, sentence_items)
       @filter_id = filter_id
       @sentence_items = sentence_items
-      filter = Struct.new("FilterID", :condition, :modifier)
-      @summary_delete  = filter.new("match:summary:.","replace:summary:")
-      @created_delete  = filter.new("match:summary:.","replace:created:")
-      @location_delete = filter.new("match:summary:.","replace:location:")
+      set_db
+      eval("@#{@filter_id} = JSON.parse(@kvs.load(@filter_id))")
     end
 
     def apply
@@ -20,11 +18,19 @@ module Goohub
 
     private
 
+    #eval では，それまでに定義していない変数名を利用できないため，あえてif文を利用している
     def expand_query(type)
-      method = eval("@#{@filter_id}.#{type}.split(':')[0]")
-      field = eval("@#{@filter_id}.#{type}.split(':')[1]")
-      value = eval("@#{@filter_id}.#{type}.split(':')[2]")
-      eval("#{method}(field,value)")
+      if type == "condition" then
+        method = eval("@#{@filter_id}['condition'].split(':')[0]")
+        field =  eval("@#{@filter_id}['condition'].split(':')[1]")
+        value =  eval("@#{@filter_id}['condition'].split(':')[2]")
+        eval("#{method}(field,value)")
+      elsif type == "modifier" then
+        method = eval("@#{@filter_id}['modifier'].split(':')[0]")
+        field =  eval("@#{@filter_id}['modifier'].split(':')[1]")
+        value =  eval("@#{@filter_id}['modifier'].split(':')[2]")
+        eval("#{method}(field,value)")
+      end
     end
 
     #####################################################
@@ -40,6 +46,29 @@ module Goohub
 
     def include?(field, str)
       @sentence_items["#{field}"].include?(str)
+    end
+
+    #####################################################
+    ### setting_methods
+    #####################################################
+    def set_db
+      summary_delete  ={
+        "condition" => "match:summary:.",
+        "modifier" => "replace:summary:"
+      }
+      created_delete  ={
+        "condition" => "match:summary:.",
+        "modifier" => "replace:created:"
+      }
+      location_delete  ={
+        "condition" => "match:summary:.",
+        "modifier" => "replace:location:"
+      }
+
+      @kvs = Goohub::DataStore.create(:redis, {:host => "localhost", :port => "6379".to_i, :db => "0".to_i})
+      @kvs.store("summary_delete", summary_delete.to_json)
+      @kvs.store("created_delete", created_delete.to_json)
+      @kvs.store("location_delete", location_delete.to_json)
     end
   end# class Filter
 end# module Goohub
