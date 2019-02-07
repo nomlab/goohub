ac8d7c39fb8194d1021c230bfd8878051847d5ad
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2019-02-07 17:28:48 +0900

    Update write command to write funnel

--- lib/goohub/command/write.rb
+++ lib/goohub/command/write.rb
@@ -2,12 +2,16 @@
 
 class GoohubCLI < Clian::Cli
   desc "write TYPE NAME QUERY", "Write NAME and QUERY into DB by TYPE"
+  option :action_name , :default => nil, :desc =>"action_name for funnel"
+  option :outlet_name , :default => nil, :desc =>"outlet_name for funnel"
   long_desc <<-LONGDESC
 TYPE is filters, actions, outlets, and funnels
 
 NAME is colum name
 
 ARG is colum query
+
+If you set `funnel` in TYPE, you must set action_name option and outlet_name option
 LONGDESC
 
   def write(type, name ,query)
@@ -48,6 +52,24 @@ LONGDESC
       outlets.uniq!
       print "Status: "
       puts kvs.store("outlets", outlets.to_json)
+
+    when "funnels" then
+      if options[:action_name] == nil or options[:outlet_name] == nil then
+        puts "If you set `funnel` in TYPE, you must set action_name option and outlet_name option!"
+        return
+      end
+      puts "Save this funnel\nname:#{name}, filter_name:#{query}, action_name:#{options[:action_name]}, outlet_name:#{options[:outlet_name]}\n"
+      funnels = JSON.parse(kvs.load("funnels"))
+      funnel = {
+        "name" => "#{name}",
+        "filter_name" => "#{query}",
+        "action_name" => "#{options[:action_name]}",
+        "outlet_name" => "#{options[:outlet_name]}"
+      }
+      funnels << funnel
+      funnels.uniq!
+      print "Status: "
+      puts kvs.store("funnels", funnels.to_json)
     else
       puts "else"
     end
