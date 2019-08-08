8d20b030a1ee3ac3cfd662c1b7f84faee2479b35
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2019-08-08 18:30:01 +0900

    Add sinatra command for goohub server
    goohubをサーバ化させるsinatra(仮名)を追加した．
    現在は，/filters, /funnels, /actions, /outlets, および/filters/name と，/info, /calendars, /caldars/name, /calendars/name/peirod をurlに配置した．

--- goohub.gemspec
+++ goohub.gemspec
@@ -27,6 +27,8 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "clian", "~> 0.3.0"
   spec.add_development_dependency "redis"
+  spec.add_development_dependency "sinatra"
+  spec.add_development_dependency "sinatra-contrib"
 
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "rake", "~> 10.0"
---
+++ lib/goohub/command/sinatra.rb
@@ -0,0 +1,104 @@
+# coding: utf-8
+require 'sinatra/base'
+require "sinatra/json"
+require "json"
+
+class GoohubCLI < Clian::Cli
+  desc "sinatra", "Work sinatra server in localhost:4567"
+
+  def sinatra
+    controller = Sinatra.new do
+      enable :logging
+      configure do
+        set (:info) {
+          settings_file_path = "settings.yml"
+          YAML.load_file(settings_file_path) if File.exist?(settings_file_path)
+        }
+
+      end
+
+      get '/' do
+        "Hello! This is goohub server"
+      end
+
+      get '/info/:key?' do
+        return json settings.info if !params["key"]
+        json settings.info[params["key"]]
+      end
+
+      get '/funnels/:name?' do
+        kvs = Goohub::DataStore.create(:file)
+        funnels = JSON.parse(kvs.load("funnels"))
+        return json funnels if !params["name"]
+        funnels.each { |f|
+          return json  f if f["name"][params["name"]]
+        }
+      end
+
+      get '/filters/:name?' do
+        kvs = Goohub::DataStore.create(:file)
+        filters = JSON.parse(kvs.load("filters"))
+        return json filters if !params["name"]
+        filters.each { |f|
+          return json  f if f["name"][params["name"]]
+        }
+      end
+
+      get '/actions/:name?' do
+        kvs = Goohub::DataStore.create(:file)
+        actions = JSON.parse(kvs.load("actions"))
+        return json actions if !params["name"]
+        actions.each { |a|
+          return json  a if a["name"][params["name"]]
+        }
+      end
+
+      get '/outlets/:name?' do
+        kvs = Goohub::DataStore.create(:file)
+        outlets = JSON.parse(kvs.load("outlets"))
+        return json outlets if !params["name"]
+        outlets.each { |o|
+          return json  o if o["name"][params["name"]]
+        }
+      end
+
+      get '/calendars/:id?' do
+        kvs = Goohub::DataStore.create(:file)
+        cal_names = []
+        kvs.keys.each { |k|
+          if k.match(/.*@.*/)
+            name = k.partition("@")[0]
+            period = k.partition("@")[2].partition("-")[2]
+            cal_names.push({name => [period]}) if cal_names == []
+            cal_names.each { |cal|
+              if cal[name]
+                cal[name].push(period)
+                cal[name].uniq!
+              else
+                cal_names.push({name => [period]})
+              end
+            }
+          end
+        }
+
+        return json cal_names if !params["id"]
+        !params["period"]
+        cal_names.each { |c|
+          return json c if c[params["id"]]
+        }
+      end
+
+      get '/calendars/:id/:period' do
+        kvs = Goohub::DataStore.create(:file)
+        kvs.keys.each { |k|
+          if k.match(/#{params['id']}.*#{params['period']}/)
+            return json JSON.parse(kvs.load(k))
+          end
+        }
+      end
+    end # Sinatra.new
+
+    controller.run!
+
+  end# method sinatra
+end# class GoohubCLI
