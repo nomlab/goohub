106f0003bfda8fffb316a78939f89e508bde5e51
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2019-08-21 14:49:05 +0900

    Update calendars command to use FileStore

--- lib/goohub/command/calendars.rb
+++ lib/goohub/command/calendars.rb
@@ -1,23 +1,29 @@
 class GoohubCLI < Clian::Cli
   desc "calendars", "List calendars"
-  option :output, :default => "stdout", :desc => "specify output destination (stdout or redis:host:port:name)"
+  option :output, :default => "stdout", :desc => "specify output destination (stdout or file or redis:host:port:name)"
 
   def calendars
     raw_resource = client.list_calendar_lists()
     calendars = Goohub::Resource::CalendarCollection.new(raw_resource)
 
     output, host, port, db_name = options[:output].split(":")
-    if output != "stdout" and (!host or !port or !db_name)
+    if output.include?("redis") and (!host or !port or !db_name)
       puts 'ERROR: "goohub events" was called with missing arguments for outputs'
       puts 'USAGE: If you want to store calendars_list to some kvs, you should set "kvs_name:hostname:port:db_name" to output'
       exit
     end
 
-    if output == "stdout"
+    case output
+    when "stdout"
       calendars.each do |item|
         puts item.summary.to_s + "(" + item.id.to_s + ")"
       end
+    when "file"
+      puts "Store calendars_list to " + output
+      kvs = Goohub::DataStore.create(:file)
+      puts kvs.store("calendars", calendars.to_json)
     else
+      # for redis
       puts "Store calendars_list to " + output
       print "Status: "
       kvs = Goohub::DataStore.create(output.intern, {:host => host, :port => port.to_i, :db => db_name.to_i})
