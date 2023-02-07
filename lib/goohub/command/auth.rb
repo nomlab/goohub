98c8f589423baf7e83ed9393a1e4f93023380928
kimura3104 <p3n85w75@s.okayama-u.ac.jp>
2023-02-07 11:50:15 +0900

    Resolve OAuth authentication issue

--- lib/goohub/command/auth.rb
+++ lib/goohub/command/auth.rb
@@ -1,3 +1,7 @@
+require 'googleauth'
+require 'webrick'
+require 'launchy'
+
 class GoohubCLI < Clian::Cli
   ################################################################
   # Command: auth
@@ -5,10 +9,58 @@ class GoohubCLI < Clian::Cli
   desc "auth", "Authenticate interactively"
   def auth
     config = @config.general
-    authorizer = Clian::Authorizer.new(config.client_id,
-                                        config.client_secret,
-                                        Google::Apis::CalendarV3::AUTH_CALENDAR,
-                                        config.token_store_path)
-    authorizer.auth_interactively(config.default_user)
+    
+    redirect_uri = 'http://localhost:8080'
+    redirect_port = 8080
+
+    authorizer = Google::Auth::UserAuthorizer.new(
+      Google::Auth::ClientId.new(config.client_id, config.client_secret),
+      Google::Apis::CalendarV3::AUTH_CALENDAR,
+      Google::Auth::Stores::FileTokenStore.new(file: config.token_store_path),
+      redirect_uri
+    )
+    url = authorizer.get_authorization_url
+    begin
+      Launchy.open(url)
+    rescue
+      puts "Open URL in your browser:\n #{url}"
+    end
+
+    auth_code = nil
+    dev_null = WEBrick::Log.new('/dev/null', 7)
+    srv = WEBrick::HTTPServer.new({ DocumentRoot: './',
+                                    BindAddress: '127.0.0.1',
+                                    Port: redirect_port,
+                                    Logger: dev_null,
+                                    AccessLog: dev_null })
+    srv.mount_proc '/' do |req, res|
+      auth_code = req.query['code']
+      res.body = res_message
+      srv.shutdown
+    end
+    srv.start
+
+    authorizer.get_and_store_credentials_from_code(
+      user_id:  config.default_user,
+      code:     auth_code
+    )
+  end
+
+  private
+
+  def res_message
+    <<~_EOT_
+    <html>
+      <head>
+        <title>goohub</title>
+      </head>
+      <body>
+        <center>
+          <h3>Success to get authentication code</h3>
+          <h4>Please close browser</h4>
+        </center>
+      </body>
+    </html>
+  _EOT_
   end
 end
